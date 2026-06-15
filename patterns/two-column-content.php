30101456eb89a3d8fda4919aace95d31429dd320
g-i-kala <5star@wp.pl>
2026-06-15 12:04:56 +0200

    refactor: prep two column layout

--- patterns/two-column-content.php
+++ patterns/two-column-content.php
@@ -7,34 +7,39 @@
  * Viewport Width: 1200
  */
 ?>
-<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
-<div class="wp-block-columns alignwide">
-	<!-- wp:column -->
-	<div class="wp-block-column">
-		<!-- wp:heading {"level":2} -->
-		<h2 class="wp-block-heading">Column Heading</h2>
-		<!-- /wp:heading -->
 
-		<!-- wp:paragraph -->
-		<p>Content for the left column. Describe your services, philosophy, or approach here.</p>
-		<!-- /wp:paragraph -->
+<!-- wp:group {"align":"full","className":"rf-two-column-content","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80","left":"var:preset|spacing|40","right":"var:preset|spacing|40"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull rf-two-column-content" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--40);">
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"var:preset|spacing|60"}}}} -->
+	<div class="wp-block-columns alignwide">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:heading {"level":2} -->
+			<h2 class="wp-block-heading">Column Heading</h2>
+			<!-- /wp:heading -->
 
-		<!-- wp:buttons -->
-		<div class="wp-block-buttons">
-			<!-- wp:button -->
-			<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#">Learn More</a></div>
-			<!-- /wp:button -->
+			<!-- wp:paragraph -->
+			<p>Content for the left column. Describe your services, philosophy, or approach here.</p>
+			<!-- /wp:paragraph -->
+
+			<!-- wp:buttons -->
+			<div class="wp-block-buttons">
+				<!-- wp:button -->
+				<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="#">Learn More</a></div>
+				<!-- /wp:button -->
+			</div>
+			<!-- /wp:buttons -->
 		</div>
-		<!-- /wp:buttons -->
-	</div>
-	<!-- /wp:column -->
+		<!-- /wp:column -->
 
-	<!-- wp:column -->
-	<div class="wp-block-column">
-		<!-- wp:image {"sizeSlug":"large"} -->
-		<figure class="wp-block-image size-large"><img src="" alt="Placeholder image" /></figure>
-		<!-- /wp:image -->
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image {"sizeSlug":"large"} -->
+			<figure class="wp-block-image size-large"><img src="" alt="Placeholder image" /></figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
 	</div>
-	<!-- /wp:column -->
+	<!-- /wp:columns -->
 </div>
-<!-- /wp:columns -->
+<!-- /wp:group -->
